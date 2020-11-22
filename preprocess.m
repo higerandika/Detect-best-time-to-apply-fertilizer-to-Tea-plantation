53a20d7465eb831180345279dd75e7182293ca68
higerandika <49017956+higerandika@users.noreply.github.com>
2020-11-22 22:35:36 +0530

    Add files via upload

---
+++ preprocess.m
@@ -0,0 +1,171 @@
+function varargout = preprocess(varargin)
+% PREPROCESS MATLAB code for preprocess.fig
+%      PREPROCESS, by itself, creates a new PREPROCESS or raises the existing
+%      singleton*.
+%
+%      H = PREPROCESS returns the handle to a new PREPROCESS or the handle to
+%      the existing singleton*.
+%
+%      PREPROCESS('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PREPROCESS.M with the given input arguments.
+%
+%      PREPROCESS('Property','Value',...) creates a new PREPROCESS or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before preprocess_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to preprocess_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help preprocess
+
+% Last Modified by GUIDE v2.5 22-Nov-2020 21:07:01
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @preprocess_OpeningFcn, ...
+                   'gui_OutputFcn',  @preprocess_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before preprocess is made visible.
+function preprocess_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to preprocess (see VARARGIN)
+
+% Choose default command line output for preprocess
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes preprocess wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = preprocess_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clc
+[filename, pathname] = uigetfile({'*.*';'*.bmp';'*.jpg';'*.gif'}, 'Pick a Tea Leaf Image File');
+I = imread([pathname,filename]);
+I = imresize(I,[256,256]);
+I2 = imresize(I,[300,400]);
+axes(handles.axes1);
+imshow(I2);title('Insert Image');
+ss = ones(300,400);
+axes(handles.axes2);
+imshow(ss);
+axes(handles.axes3);
+imshow(ss);
+handles.ImgData1 = I;
+guidata(hObject,handles);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%clear all
+%close all
+I3 = handles.ImgData1;
+I4 = imadjust(I3,stretchlim(I3));
+I5 = imresize(I4,[300,400]);
+axes(handles.axes2);
+imshow(I5);title(' Contrast Enhanced ');
+handles.ImgData2 = I4;
+guidata(hObject,handles);
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+I3 = handles.ImgData1;
+I4 = imadjust(I3,stretchlim(I3));
+I5 = imresize(I4,[300,400]);
+gray = rgb2gray(I5);
+imhist(gray);
+imhist(gray);
+axes(handles.axes3);
+imshow(gray);title(' Gray Scale ');
+handles.ImgData3 = gray;
+guidata(hObject,handles);
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Extract the target region
+
+I6 = handles.ImgData3;
+I7 = imnoise(I6,'gaussian',0,0.025);
+I8 = imresize(I7,[300,400]);
+axes(handles.axes4);
+imshow(I8);title('Added Gaussian Noise');
+handles.ImgData4 = I7   ;
+guidata(hObject,handles);
+
+% --- Executes on button press in pushbutton3.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+I9 = handles.ImgData4;
+I10 = wiener2(I9,[5 5]);
+I11 = imresize(I10,[300,400]);
+axes(handles.axes5);
+imshow(I11);title('Noise Removed by Wiener Filter');
+handles.ImgData5 = I10   ;
+guidata(hObject,handles);
+
+% --- Executes on button press in pushbutton4.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+I3 = handles.ImgData1;
+I4 = imadjust(I3,stretchlim(I3));
+I5 = imresize(I4,[300,400]);
+axes(handles.axes6);
+imshow(I5);title(' Contrast Enhanced ');
+handles.ImgData6 = I4;
+guidata(hObject,handles);
